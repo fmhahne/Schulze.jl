f1cef062871e0aa434f83f7625be8555c2a86977
Fernando Miguel Hahne <fernandomhahne@gmail.com>
2022-10-12 21:05:15 -0300

    Blue style

--- src/Schulze.jl
+++ src/Schulze.jl
@@ -6,11 +6,11 @@ function schulze(winningvotes)
     p = ifelse.(winningvotes .> winningvotes', winningvotes, 0)
     candidates = axes(winningvotes, 1)
 
-    for i ∈ candidates
-        for j ∈ candidates
-            if i ≠ j
-                for k ∈ candidates
-                    if i ≠ k && j ≠ k
+    for i in candidates
+        for j in candidates
+            if i != j
+                for k in candidates
+                    if i != k && j != k
                         p[j, k] = max(p[j, k], min(p[j, i], p[i, k]))
                     end
                 end
@@ -18,6 +18,6 @@ function schulze(winningvotes)
         end
     end
 
-    return sort(candidates, lt=(i, j) -> p[i, j] > p[j, i])
+    return sort(candidates; lt=(i, j) -> p[i, j] > p[j, i])
 end
 end
